579d44205674f3a7c097879a3ac9be63d1321be6
mw5868 <30810584+mw5868@users.noreply.github.com>
2017-12-14 12:14:00 +0000

    Update Basic_Code.py

--- Basic_Code.py
+++ Basic_Code.py
@@ -1,1 +1,9 @@
-
+from numpy import radians
+from scipy.constants import kilo
+from orbital import earth, KeplerianElements, plot, earth_sidereal_day, Maneuver,plot3d
+# Define the orbital parameters
+orbit3 = KeplerianElements.with_altitude(1000*kilo,body=earth, e=0.5)
+# Define the maneuver type, and parameters
+man7 = Maneuver.hohmann_transfer_to_altitude(10000*kilo)
+# Plots in 3d the orbital parameters, including the defined maneuver.
+plot3d(orbit3,title='Maneuver',maneuver=man7)
