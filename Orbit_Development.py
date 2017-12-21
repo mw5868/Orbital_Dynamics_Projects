18e0a52b7f6d182680fe8f55144ee5b7b115d6d3
mw5868 <30810584+mw5868@users.noreply.github.com>
2017-12-21 20:37:14 +0000

    minor changes to code

--- Orbit_Development.py
+++ Orbit_Development.py
@@ -5,12 +5,9 @@ Created on Thu Dec 21 19:53:22 2017
 @author: mwood
 """
 
-from orbital import KeplerianElements, earth,plot,plot3d
+from orbital import utilities as ut
 import matplotlib.pyplot as plt
 from scipy.constants import kilo
 
-flight = KeplerianElements.with_altitude(5000*kilo, body = earth)
-plot(flight)
-plt.show()
-plot3d(flight)
-plt.show()
+radius = ut.altitude_from_radius(5000*kilo, body=earth)
+print(radius)
